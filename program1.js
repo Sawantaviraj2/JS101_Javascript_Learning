9b5e1c6dc757f6ea444bd5a80d2a3b81fcd5f1bb
Sawantaviraj2 <121309811+Sawantaviraj2@users.noreply.github.com>
2022-12-30 16:13:15 +0530

    Add files via upload

--- program1.js
+++ program1.js
@@ -1,2 +1,10 @@
-let c=10;
-console.log(c);
+let student ={
+  name:"Aniket",
+  age:25,
+  phone:62846521354,
+  father_name: "Goku",
+  mother_name:"Nami",
+};
+console.log(student);
+console.log(student.Name);
+console.log(student["Name"]);
--- program2.js
+++ program2.js
@@ -1,4 +1,8 @@
-var b=40;
-console.log(b);
-b=20;
-console.log(b);
+let details={
+  name:"Aniket",
+  age:25,
+  phone:62846521354,
+  father_name: "Goku",
+  mother_name:"Nami",
+};
+console.log(details);
